bc2fc2d709fd95202a0c3497f38233481b13a49c
lhg010423 <lhg010423@gmail.com>
2024-04-24 12:32:27 +0900

    240424

---
+++ Seed/src/test/java/seed/project/SeedApplicationTests.java
@@ -0,0 +1,13 @@
+package seed.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
