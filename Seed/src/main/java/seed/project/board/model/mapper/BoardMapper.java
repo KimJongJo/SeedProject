53e654893e28ec653c5b7db0941eb12a7e145569
lhg010423 <lhg010423@gmail.com>
2024-05-09 17:01:06 +0900

    56341531

--- Seed/src/main/java/seed/project/board/model/mapper/BoardMapper.java
+++ Seed/src/main/java/seed/project/board/model/mapper/BoardMapper.java
@@ -151,4 +151,21 @@ public interface BoardMapper {
 	List<Board> getSearchCount2(Map<String, Object> paramMap, RowBounds rowBounds);
 
 
+	/** [1] 게시글 수정하기
+	 * @param board
+	 * @return
+	 */
+	int board1Update(Map<String, Object> board);
+
+
+	/** [1] 게시글 삭제하기
+	 * @param boardNo
+	 * @return
+	 */
+	int board1Delete(int boardNo);
+
+
+
+
+
 }
